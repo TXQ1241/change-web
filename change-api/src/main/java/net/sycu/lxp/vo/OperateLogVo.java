b1de60c0d1e54dacf9b38f450ff5c4fa77cda724
ferry wolf <wsx1993lh2013@163.com>
2018-05-03 02:27:14 +0800

    1

--- change-api/src/main/java/net/sycu/lxp/vo/OperateLogVo.java
+++ change-api/src/main/java/net/sycu/lxp/vo/OperateLogVo.java
@@ -2,7 +2,26 @@ package net.sycu.lxp.vo;
 
 public class OperateLogVo extends PageVo {
 	
+	private String userName;
+	private String account;
 	private String operateType;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
 
 	public String getOperateType() {
 		return operateType;
