f56686611a8800b0b594df59d744b657b1e11b39
yangxingkang <yangxingkang_gz@163.com>
2018-05-03 16:19:25 +0800

    5

--- change-web/src/main/webapp/js/admin/passwordChangeLog.js
+++ change-web/src/main/webapp/js/admin/passwordChangeLog.js
@@ -218,7 +218,7 @@ layui.use(['table'], function () {
     $('#goosEdit').on('click', function () {
         window.location.href = window.location.origin + window.location.pathname + '?userType=3';
     });
-    $('#passworkChangeLog').on('click', function () {
-        window.location.href = window.location.origin + window.location.pathname + '?userType=5';
+    $('#loginLog').on('click', function () {
+        window.location.href = window.location.origin + window.location.pathname + '?userType=4';
     });
 });
